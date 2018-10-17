600e98216ab765b758acf181bb2dec9df452f2dc
em <oupquv@gmail.com>
2018-10-17 19:36:57 +0800

    add html&css

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main class="content">
+      <div class="add">
+        <h2>新增單筆：</h2>
+        <div>
+          <ul>
+            <li>
+              <input type="text" placeholder="名稱">
+              <span></span>
+            </li>
+            <li>
+              <input type="text" placeholder="單價">
+              <span></span>
+            </li>
+            <li>
+              <input type="text" placeholder="數量">
+              <span></span>
+            </li>
+          </ul>
+          <button>新增</button>
+        </div>
+      </div>
+      <div class="bag">
+        <h2>購物清單：</h2>
+        <table>
+          <tbody>
+            <tr>
+              <th>No</th>
+              <th>名稱</th>
+              <th>單價</th>
+              <th>數量</th>
+              <th></th>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Apple IPhone 8 64G</td>
+              <td>18999</td>
+              <td>2</td>
+              <td>
+                <button>刪除</button>
+              </td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>小米行動電源2C</td>
+              <td>619</td>
+              <td>2</td>
+              <td>
+                <button>刪除</button>
+              </td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>NIKE BENASSI JDI 拖鞋</td>
+              <td>499</td>
+              <td>2</td>
+              <td>
+                <button>刪除</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div class="total">總價： 99999</div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
