c5d0cc561301135d85a8be7dabb9ce08d80e5646
em <oupquv@gmail.com>
2018-10-18 15:23:37 +0800

    add check action & message

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -5,19 +5,20 @@
       <div>
         <ul>
           <li>
-            <input v-model="title" type="text" placeholder="名稱">
+            <input v-model="title" :class="{ checkInput : this.title }" type="text" placeholder="名稱">
             <span></span>
           </li>
           <li>
-            <input v-model="price" type="text" placeholder="單價">
+            <input v-model="price" :class="{ checkInput : this.price }" type="text" placeholder="單價">
             <span></span>
           </li>
           <li>
-            <input v-model="num" type="text" placeholder="數量">
+            <input v-model="num" :class="{ checkInput : this.num }" type="text" placeholder="數量">
             <span></span>
           </li>
         </ul>
         <button @click="addBtn">新增</button>
+        <span class="warn">{{warnMessage}}</span>
       </div>
     </div>
     <div class="bag">
@@ -55,6 +56,7 @@ export default {
       title: '',
       price: '',
       num: '',
+      warnMessage:'',
       lists: [
         { title: '法蘭絨格紋襯衫', price: 790, num: 1 },
         { title: '羊毛法蘭絨混紡 SLIM FIT外套', price: 3990 , num: 2 },
@@ -74,17 +76,40 @@ export default {
   },
   methods:{
     addBtn(){
-      if( this.title != '' && this.price != '' && this.num != '' ){
-        this.lists.push({title:this.title, price:Number(this.price), num:Number(this.num)})   
+      if( this.title == '' || this.price == '' || this.num == '' || isNaN(this.price) || isNaN(this.num) ){
+        this.warnMessage = '* 請完整與正確填入新增資訊。'
+      }else if( !Number.isInteger(Number(this.price)) || !Number.isInteger(Number(this.num)) ){
+        this.warnMessage = '* 價格或數量請輸入整數。'
+      }else{
+        let titleArray = [],
+            priceArray = [],
+            reTitle,
+            rePrice,
+            listsLength = this.lists.length
+        
+        for( let i = 0; i < listsLength; i++ ){
+          titleArray.push(this.lists[i].title)
+          priceArray.push(this.lists[i].price)
+        }
+        reTitle = titleArray.indexOf(this.title)
+        rePrice = priceArray.indexOf(Number(this.price))
+
+        if( reTitle != -1 && rePrice != -1){
+          this.lists[reTitle].num = this.lists[reTitle].num + Number(this.num)  
+        }else{
+          this.lists.push({ title: this.title, price: Number(this.price), num: Number(this.num) })
+        }
+
         this.title = '',
         this.price = '',
         this.num = ''
-      }else{
-        alert('請完整輸入新增資訊。')
       }
     },
     removeBtn(index){
-      this.lists.splice(index,1)
+      let a = confirm("是否確定刪除？");
+      if (a == true) {
+        this.lists.splice(index,1)
+      } 
     }
   }
 }
